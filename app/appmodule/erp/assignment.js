a442d29e142c23718e437d3a5152973316536ed7
vivekpandey12 <vivek.pandey5188@gmail.com>
2017-09-13 18:25:22 +0530

    service committed

--- app/appmodule/erp/assignment.js
+++ app/appmodule/erp/assignment.js
@@ -3,10 +3,22 @@ var rs = require("gen").res;
 var globals = require("gen").globals;
 
 var assignment = module.exports = {};
+var tripapi = require("../schoolapi/tripapi.js");
 
 assignment.saveAssignmentInfo = function saveAssignmentInfo(req, res, done) {
     db.callFunction("select " + globals.erpschema("funsave_assignmentinfo") + "($1::json);", [req.body], function(data) {
         rs.resp(res, 200, data.rows);
+
+        var _dtr = {
+            "flag": "parents_notification",
+            "title": req.body.title,
+            "body": req.body.msg,
+            "parentsid": data.rows[0].funsave_assignmentinfo.parentsid
+        }
+
+        console.log(_dtr);
+
+        tripapi.sendNotification(_dtr);
     }, function(err) {
         rs.resp(res, 401, "error : " + err);
     })
