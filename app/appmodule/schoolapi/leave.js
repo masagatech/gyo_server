d3532351b982c1a42fef90203b1a1f2216810779
vivekpandey12 <vivek.pandey5188@gmail.com>
2017-09-21 22:12:14 +0530

    all service is committed

--- app/appmodule/schoolapi/leave.js
+++ app/appmodule/schoolapi/leave.js
@@ -6,7 +6,7 @@ var lvpsngr = module.exports = {};
 var tripapi = require("../schoolapi/tripapi.js");
 
 lvpsngr.savePassengerLeave = function savePassengerLeave(req, res, done) {
-    db.callFunction("select " + globals.schema("funsave_studentleave") + "($1::json);", [req.body], function(data) {
+    db.callFunction("select " + globals.schema("funsave_leaveinfo") + "($1::json);", [req.body], function(data) {
         rs.resp(res, 200, data.rows);
     }, function(err) {
         rs.resp(res, 401, "error : " + err);
@@ -14,7 +14,7 @@ lvpsngr.savePassengerLeave = function savePassengerLeave(req, res, done) {
 }
 
 lvpsngr.getPassengerLeave = function getPassengerLeave(req, res, done) {
-    db.callProcedure("select " + globals.schema("funget_studentleave") + "($1,$2::json);", ['lp', req.body], function(data) {
+    db.callProcedure("select " + globals.schema("funget_leavedetails") + "($1,$2::json);", ['lp', req.body], function(data) {
         rs.resp(res, 200, data.rows);
     }, function(err) {
         rs.resp(res, 401, "error : " + err);
@@ -22,14 +22,14 @@ lvpsngr.getPassengerLeave = function getPassengerLeave(req, res, done) {
 }
 
 lvpsngr.savePassengerLeaveApproval = function savePassengerLeaveApproval(req, res, done) {
-    db.callFunction("select " + globals.schema("funsave_studentleaveapproval") + "($1::json);", [req.body], function(data) {
+    db.callFunction("select " + globals.schema("funsave_leaveapproval") + "($1::json);", [req.body], function(data) {
         rs.resp(res, 200, data.rows);
 
         var _dtr = {
             "flag": "notification",
-            "title": data.rows[0].funsave_studentleaveapproval.title,
-            "body": data.rows[0].funsave_studentleaveapproval.msg,
-            "psngrid": data.rows[0].funsave_studentleaveapproval.ntfpsngrid
+            "title": data.rows[0].funsave_leaveapproval.title,
+            "body": data.rows[0].funsave_leaveapproval.msg,
+            "psngrid": data.rows[0].funsave_leaveapproval.ntfpsngrid
         }
 
         tripapi.sendNotification(_dtr);
