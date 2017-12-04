4517fea96522cc9e10876252396c7f11755e681c
vivekpandey12 <vivek.pandey5188@gmail.com>
2017-12-04 21:18:00 +0530

    committed

--- app/appmodule/erp/attendance.js
+++ app/appmodule/erp/attendance.js
@@ -3,10 +3,22 @@ var rs = require("gen").res;
 var globals = require("gen").globals;
 
 var attnd = module.exports = {};
+var tripapi = require("../schoolapi/tripapi.js");
 
 attnd.saveAttendance = function saveAttendance(req, res, done) {
     db.callFunction("select " + globals.erpschema("funsave_attendance") + "($1::json);", [req.body], function(data) {
         rs.resp(res, 200, data.rows);
+
+        var _dtr = {
+            "flag": "parents_notification",
+            "title": data.rows[0].funsave_attendance.ntftitle,
+            "body": data.rows[0].funsave_attendance.ntfmsg,
+            "parentsid": data.rows[0].funsave_attendance.parentsid
+        }
+
+        console.log(_dtr);
+
+        tripapi.sendNotification(_dtr);
     }, function(err) {
         rs.resp(res, 401, "error : " + err);
     })
