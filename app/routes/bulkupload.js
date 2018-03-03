d4d6cb9ec2af673304e460004fcefe3482d801e9
vivekpandey12 <vivek.pandey5188@gmail.com>
2018-03-03 18:29:03 +0530

    committed

--- app/routes/bulkupload.js
+++ app/routes/bulkupload.js
@@ -26,6 +26,32 @@ var appRouter = function(app) {
 
     /** API path that will upload the files */
 
+    function checkDuplicates(arr, justCheck) {
+        var len = arr.length,
+            tmp = {},
+            arrtmp = arr.slice(),
+            dupes = [];
+
+        arrtmp.sort();
+
+        while (len--) {
+            var val = arrtmp[len];
+
+            if (/nul|nan|infini/i.test(String(val))) {
+                val = String(val);
+            }
+
+            if (tmp[JSON.stringify(val)]) {
+                if (justCheck) { return true; }
+                dupes.push(val);
+            }
+
+            tmp[JSON.stringify(val)] = true;
+        }
+
+        return justCheck ? false : dupes.length ? dupes : null;
+    }
+
     app.post(globals.globvar.rootAPI + '/bulkUpload', upload.any(), function(req, res) {
         var exceltojson; // Initialization
 
@@ -59,22 +85,88 @@ var appRouter = function(app) {
 
                     }
 
-                    console.log(result);
+                    var _status = 1;
+                    var _message = '';
 
                     if (req.body.bulktype === "student") {
-                        var params = {
-                            "ayid": req.body.ayid,
-                            "enttid": req.body.enttid,
-                            "wsautoid": req.body.wsautoid,
-                            "cuid": req.body.cuid,
-                            "multistudent": result
-                        };
+                        if (checkDuplicates(result, true)) {
+                            _status = 0;
+                            _message = "Duplicate Records Not Allowed";
+                        } else {
+                            for (var i = 0; i < result.length; i++) {
+                                if (result[i].first_name == "") {
+                                    _status = 0;
+                                    _message = "Empty First Name";
 
-                        console.log(params);
+                                    break;
+                                } else if (result[i].middle_name == "") {
+                                    _status = 0;
+                                    _message = "Empty Middle Name";
 
-                        admsn.saveMultiStudentInfo(params, function(d) {
-                            res.json({ data: d });
-                        });
+                                    break;
+                                } else if (result[i].last_name == "") {
+                                    _status = 0;
+                                    _message = "Empty Last Name";
+
+                                    break;
+                                } else if (result[i].roll_no == "") {
+                                    _status = 0;
+                                    _message = "Empty Roll No In " + result[i].first_name;
+
+                                    break;
+                                } else if (result[i].class_name == "") {
+                                    _status = 0;
+                                    _message = "Empty Class Name In " + result[i].first_name;
+
+                                    break;
+                                } else if (result[i].dob == "") {
+                                    _status = 0;
+                                    _message = "Empty DOB In " + result[i].first_name;
+
+                                    break;
+                                } else if (result[i].address == "") {
+                                    _status = 0;
+                                    _message = "Empty Address In " + result[i].first_name;
+
+                                    break;
+                                } else if (result[i].father_name == "") {
+                                    _status = 0;
+                                    _message = "Empty Father Name In " + result[i].first_name;
+
+                                    break;
+                                } else if (result[i].father_mobile == "") {
+                                    _status = 0;
+                                    _message = "Empty Father Mobile In " + result[i].first_name;
+
+                                    break;
+                                } else if (result[i].father_email == "") {
+                                    _status = 0;
+                                    _message = "Empty Father Email In " + result[i].first_name;
+
+                                    break;
+                                } else {
+                                    _status = 1;
+                                    _message = "";
+                                }
+                            }
+                        }
+
+                        if (_status == 1) {
+                            var params = {
+                                "ayid": req.body.ayid,
+                                "enttid": req.body.enttid,
+                                "wsautoid": req.body.wsautoid,
+                                "cuid": req.body.cuid,
+                                "multistudent": result
+                            };
+
+                            admsn.saveMultiStudentInfo(params, function(data) {
+                                res.json({ status: _status, message: _message, data: data });
+                            });
+                        } else {
+                            res.json({ status: _status, message: _message, data: null });
+                            // res.send(500, _message);
+                        }
                     }
                 });
             } catch (e) {
