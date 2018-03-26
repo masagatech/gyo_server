0cd8f3d4adf48ad6a2de015c4b66b2df913285b6
vivekpandey12 <vivek.pandey5188@gmail.com>
2018-03-26 23:55:01 +0530

    committed

--- app/reports/templates/announcement/announcement.js
+++ app/reports/templates/announcement/announcement.js
@@ -3,12 +3,12 @@ var reports = module.exports = {};
 
 reports.getAnnouncementReports = function getAnnouncementReports(data) {
     var _hndlbar = Handlebars;
-    var adt = data.data;
+    var announcementdt = data.data;
 
     var col_total = [];
 
     _hndlbar.registerHelper('showdata', function(params) {
-        if (mileagedt.length == 0) {
+        if (announcementdt.length == 0) {
             return "hide";
         } else {
             return "show";
@@ -16,7 +16,7 @@ reports.getAnnouncementReports = function getAnnouncementReports(data) {
     });
 
     _hndlbar.registerHelper('emptydatamsg', function(params) {
-        if (mileagedt.length == 0) {
+        if (announcementdt.length == 0) {
             return "No Data Found";
         } else {
             return "";
--- app/reports/templates/notification/notification.js
+++ app/reports/templates/notification/notification.js
@@ -3,12 +3,12 @@ var reports = module.exports = {};
 
 reports.getNotificationReports = function getNotificationReports(data) {
     var _hndlbar = Handlebars;
-    var mileagedt = data.data;
+    var notificationdt = data.data;
 
     var col_total = [];
 
     _hndlbar.registerHelper('showdata', function(params) {
-        if (mileagedt.length == 0) {
+        if (notificationdt.length == 0) {
             return "hide";
         } else {
             return "show";
@@ -16,7 +16,7 @@ reports.getNotificationReports = function getNotificationReports(data) {
     });
 
     _hndlbar.registerHelper('emptydatamsg', function(params) {
-        if (mileagedt.length == 0) {
+        if (notificationdt.length == 0) {
             return "No Data Found";
         } else {
             return "";
