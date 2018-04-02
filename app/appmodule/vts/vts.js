f1b5752739123bfba4fa613273f0036ae1da661f
vivekpandey12 <vivek.pandey5188@gmail.com>
2018-04-03 02:22:56 +0530

    committed

--- app/appmodule/vts/vts.js
+++ app/appmodule/vts/vts.js
@@ -5,21 +5,12 @@ var globals = require("gen").globals;
 var tripapi = require("../schoolapi/tripapi.js");
 var vts = module.exports = {};
 
-function saveNotification(req, res, _data) {
+function saveNotification(req, res, _remark3, _data) {
     var params = {
         "grpid": 0,
         "frmid": 1,
         "frmtype": "admin",
-        "remark3": {
-            "almtyp": req.query.almtyp,
-            "btcid": req.query.batchid,
-            "drvid": req.query.drvid,
-            "vehid": req.query.vehid,
-            "rtid": req.query.rtid,
-            "stpid": req.query.stpid,
-            "stptype": req.query.stptype,
-            "pdtype": req.query.pdtype
-        },
+        "remark3": _remark3,
         "studid": _data.studid,
         "title": _data.title,
         "msg": _data.msg,
@@ -40,7 +31,7 @@ function saveNotification(req, res, _data) {
 
 vts.getFence = function(req, res, done) {
     var ntfparams = {
-        "flag": "notification",
+        "flag": req.query.almtyp,
         "almtyp": req.query.almtyp,
         "batchid": req.query.batchid,
         "drvid": req.query.drvid,
@@ -52,27 +43,15 @@ vts.getFence = function(req, res, done) {
         "enttid": req.query.enttid
     };
 
-    db.callProcedure("select " + globals.schema("funget_pickdropdetails") + "($1,$2::json);", ['ntf', ntfparams], function(data) {
+    db.callProcedure("select " + globals.schema("funapisend_notification_or_not") + "($1,$2::json);", ['ntf', ntfparams], function(data) {
         var _data = data.rows[0];
 
         if (_data.status == 0) {
             rs.resp(res, 200, _data);
         } else {
-            saveNotification(req, res, _data);
+            saveNotification(req, res, ntfparams, _data);
 
-            var dparams = {
-                "flag": req.query.almtyp,
-                "enttid": req.query.enttid,
-                "batchid": req.query.batchid,
-                "drvid": req.query.drvid,
-                "vehid": req.query.vehid,
-                "rtid": req.query.rtid,
-                "stpid": req.query.stpid,
-                "stptype": req.query.stptype,
-                "pdtype": req.query.pdtype
-            };
-
-            tripapi.sendNotification(dparams);
+            tripapi.sendNotification(ntfparams);
         }
     }, function(err) {
         rs.resp(res, 401, "error : " + err);
