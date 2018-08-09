c131c7c1eee31cb7b8c94409af7820b71d0e4a83
vivekpandey12 <vivek.pandey5188@gmail.com>
2018-08-09 20:32:54 +0530

    committed

--- app/appmodule/schoolapi/dashboard.js
+++ app/appmodule/schoolapi/dashboard.js
@@ -13,9 +13,9 @@ dashboard.getDashboard = function getDashboard(req, res, done) {
 }
 
 dashboard.getHelpDesk = function getHelpDesk(req, res, done) {
-    db.callProcedure("select " + globals.schema("funget_helpdesk") + "($1,$2::json);", ['hd', req.body], function(data) {
+    db.callProcedure("select " + globals.schema("funget_helpdesk") + "($1,$2,$3,$4::json);", ['hd1', 'hd2', 'hd3', req.body], function(data) {
         rs.resp(res, 200, data.rows);
     }, function(err) {
         rs.resp(res, 401, "error : " + err);
-    }, 1)
+    }, 3)
 }
